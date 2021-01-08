369f787c23740971f39ac3e49691ea6a3b1e6aca
jordles113 <jordan.t.romero@gmail.com>
2021-01-07 20:19:48 -0600

    builds out sprite/index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sprite = () => {
+    return (
+        <div>
+            style={{
+                display: 'inline-block',
+                height: '32px',
+                width: '32px',
+                backgroundImage: 'url(/sprites/skins/m1.png)',
+                backgroundRepeat: 'no-repeat' 
+            }}
+        </div>
+    )
+}
+
+export default Sprite
